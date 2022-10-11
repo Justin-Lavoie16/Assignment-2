8af1fb75afb372538c0eb09de223f8bdc5cb7724
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-10-11 18:28:14 +0000

    fixed everything

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function calculate () {
+  
+  const radius = parseInt(document.getElementById('radius-of-circle').value)
+
+
+  const area = radius**2 * 3.14159
+
+  
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
